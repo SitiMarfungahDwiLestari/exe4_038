ead3173704cd62d2fab050dea3d0d560a34080f2
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2022-12-09 15:56:22 +0700

    add class Stack

--- exe4_038/Program.cs
+++ exe4_038/Program.cs
@@ -12,6 +12,19 @@ namespace exe4_038
             next = n;
         }
     }
+    class Stacks
+    {
+        Node top;
+        public string[] siti;
+        public int max;
+        public int info;
+        public Stacks()
+        {
+            siti = new string[58];
+            info = -1;
+            max = 58;
+        }
+    }
     /*static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
